3454dc126510f4fa19b78b1e3951ff4cedd5f6df
hristopher <hristopher@sourcelab.bg>
2024-04-21 22:45:18 +0300

    feat: add router and react querry

---
+++ templates/react-ts-tailwind/src/components/home-title.tsx
@@ -0,0 +1,5 @@
+const HomeTitle = () => {
+  return <h1 className="text-2xl">React + Vite + TS</h1>
+}
+
+export default HomeTitle
--- templates/react-ts-tailwind/vite.config.ts
+++ templates/react-ts-tailwind/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    server: {
+      /* ------- Configure the server ------- */
+      // proxy: {
+      //   '/api': {
+      //     target: 'http://localhost:5000/api',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      //   },
+      // },
+    },
+  },
 })
